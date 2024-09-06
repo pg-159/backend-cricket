122036525d589b20f879c9b1418df620efe43dbb
pg-159 <pratap.gadhvi159@gmail.com>
2024-09-06 16:10:26 +0530

    feat: adding backend players apis for get all players and get player by id

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+const players = [
+  {
+    id: 1,
+    name: "Virat Kohli",
+    country: "India",
+    role: "Batsman",
+    runs: 12000,
+    wickets: 4,
+  },
+  {
+    id: 2,
+    name: "James Anderson",
+    country: "England",
+    role: "Bowler",
+    runs: 1243,
+    wickets: 600,
+  },
+  {
+    id: 3,
+    name: "Kane Williamson",
+    country: "New Zealand",
+    role: "Batsman",
+    runs: 7000,
+    wickets: 29,
+  },
+  {
+    id: 4,
+    name: "Pat Cummins",
+    country: "Australia",
+    role: "Bowler",
+    runs: 750,
+    wickets: 250,
+  },
+  {
+    id: 5,
+    name: "Ben Stokes",
+    country: "England",
+    role: "All-rounder",
+    runs: 4500,
+    wickets: 150,
+  },
+];
+
+app.get("/players", (req, res) => {
+  res.json({ players });
+});
+
+app.get("/players/:id", (req, res) => {
+  let id = req.params.id;
+  let result = players.find((player) => player.id == id);
+  return res.json({ result });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
